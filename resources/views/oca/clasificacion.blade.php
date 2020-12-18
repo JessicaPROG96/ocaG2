72ead65a47f5ebe3393d2c2bb008fdcf1149d7ec
jessica <jesi7gmar@gmail.com>
2020-12-18 11:47:24 +0100

    plantilla

---
+++ resources/views/oca/clasificacion.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo asset('css/inicio.css')?>" type="text/css">
+@extends('layouts.master')
+@section('content')
+<h1>Clasificación</h1>
+@stop
---
+++ resources/views/oca/galeria.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo asset('css/inicio.css')?>" type="text/css">
+@extends('layouts.master')
+@section('content')
+<h1>Espacio de la galeria</h1>
+@stop
