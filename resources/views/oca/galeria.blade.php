7c247345121a650740fc97b2437524d8c9151488
EricEguski <ericeguskizaga@gmail.com>
2020-12-18 14:11:34 +0100

    update galeria

--- resources/views/oca/galeria.blade.php
+++ resources/views/oca/galeria.blade.php
@@ -5,18 +5,40 @@
 <!-- El titulo de la galeria -->
     <h1 class="galeria">Galeria</h1>
 
+    <!-- Search form -->
+<div class="col-md-10 md-form mb-4 mr-auto ml-auto">
+    <input class="form-control" type="text" placeholder="Buscar en nuestra galeria" aria-label="Search">
+  </div>
+
+<!-- El div de la galeria -->
+    <div class="divGaleria col-md-10">
+
 <!-- Por cada mujer que hay en la base de datos creamos un div con los datos -->
 <!-- $mujeres = todas las mujeres de la base de datos y la $a = cada mujer -->
-    <div class="col-md-12">
-    <!-- hay que hacer un row que tenga 4 mujeres hasta que se acabe-->
+
+        {{-- @for($i=0;$i<=5;$i++) --}}
+
+            <div class="row espacio">
+
+                @php $contador = 0; @endphp <!-- Creamos la variable para romper el foreach -->
+                                
+                @foreach( $mujeres as $key => $a )
+                {{-- @for($i=0;$i<=5;$i++) --}}
+                    <div class="mujer">
+                        <img class="imagen" src="./../resources/img/fotosMujeres/{{$a['imagen']}}" style="height:200px"/>
+                        <h4 class="nombre">{{$a['nombre']}}</h4>
+                        <h4 class="Apellido">{{$a['apellido']}}</h4>
+                    </div>
+                
+                    {{-- @php $contador++ @endphp 
+                    @if ($contador == 5) <!-- Cada vez que se impriman 3 mujeres, salimos del foreach -->
+                        
+                        @php break; @endphp
+                    @endif --}}
+                @endforeach
+            </div>
+        {{-- @endfor --}}
     
-    @foreach( $mujeres as $key => $a )
-        <div class="mujer">
-            <img class="imagen" src="./../resources/img/fotosMujeres/{{$a['imagen']}}" style="height:200px"/>
-            <h4 class="nombre">{{$a['nombre']}}</h4>
-            <h4 class="Apellido">{{$a['apellido']}}</h4>
-        </div>
-    @endforeach
     </div>
     
     
