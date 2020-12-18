b76f861fcaffdeeaa3dbfb19ebf72f673fd66d0a
jessica <jesi7gmar@gmail.com>
2020-12-18 13:40:14 +0100

    login

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,11 @@
 <head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
+  <script src="{{ asset('js/app.js') }}" defer></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="<?php echo asset('css/plantilla.css') ?>" type="text/css">
 
   <title>Hello, world!</title>
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [ocaController::class, 'index']); 
+ 
 // Route::get('/', 'Auth\ocaController@index')->name('login');
+Auth::routes();
+
+Route::get('/', [ocaController::class, 'index']);
+
+// Route::get('/', 'ocaController@index');
 
 Route::get('juego', [ocaController::class, 'tablero']);
 
@@ -27,6 +32,6 @@ Route::get('clasificacion', [ocaController::class, 'clasificacion']);
 //     return view('welcome');
 // });
 
-// Auth::routes();
+
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
