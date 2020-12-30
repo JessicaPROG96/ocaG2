ab7cb49369918f8251e8ef7b766131f58f529695
Jessica <jesi7gmar@gmail.com>
2020-12-30 12:40:19 +0100

    tablero

--- resources/views/oca/tablero.blade.php
+++ resources/views/oca/tablero.blade.php
@@ -1,6 +1,23 @@
 <link rel="stylesheet" href="<?php echo asset('css/inicio.css') ?>" type="text/css">
 @extends('layouts.master')
 @section('content')
+<div class="jugador">
+    <div class="row row-cols-1">
+        <div class="col">
+            <img src="../resources/img/avatars/default.png" alt="No disponible" width="30px" height="30px">
+            <h6>Jugador 1</h6>
+        </div>
+        <div class="col"><img src="../resources/img/avatars/default.png" alt="No disponible" width="30px" height="30px">
+            <h6>Jugador 2</h6>
+        </div>
+        <div class="col"><img src="../resources/img/avatars/default.png" alt="No disponible" width="30px" height="30px">
+            <h6>Jugador 3</h6>
+        </div>
+        <div class="col"><img src="../resources/img/avatars/default.png" alt="No disponible" width="30px" height="30px">
+            <h6>Jugador 4</h6>
+        </div>
+    </div>
+    </div>
 <div> 
     <tablero-component></tablero-component>
 </div>
