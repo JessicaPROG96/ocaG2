6a57fce712ee8e12b45a7d511edafd93ea6ca801
EricEguski <ericeguskizaga@gmail.com>
2021-01-07 13:42:27 +0100

    Buscar WIP

--- public/js/galeria.js
+++ public/js/galeria.js
@@ -1,35 +1,22 @@
 // Este script es para la pagina de la galeria de las mujeres
 
-// function buscar(){ 
-//     let input = document.getElementById('searchbar').value 
-//     let x = document.getElementsByClassName('nombre'); 
-//     for (i = 0; i < x.length; i++) {  
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-//             x[i].style.display="none"; 
-//         } 
-//         else { 
-//             x[i].style.display="list-item";                  
-//         } 
-//     } 
-
-// };
 function b() {
     let input = document.getElementById('searchbar').value;
     let div = document.getElementsByClassName('mujer');
-    let h4 = document.getElementsByClassName('nombre');
+    let nom = document.getElementsByClassName('nombre');
+    let ape = document.getElementsByClassName('Apellido');
     for (i = 0; i < div.length; i++) {  
-                if (!h4[i].innerHTML.includes(input)) { 
+                if (!nom[i].innerHTML.includes(input) && !ape[i].innerHTML.includes(input)) { 
                     div[i].style.display="none"; 
                 } 
                 else { 
-                    div[i].style.display="list-item";                  
+                    div[i].style.display="block";                  
                 } 
             } 
 }
 
 
-function a() {
-    // $("#searchbar").on( "change",'input', b);           
+function a() {          
     $("#searchbar").change(b);           
 };
 
