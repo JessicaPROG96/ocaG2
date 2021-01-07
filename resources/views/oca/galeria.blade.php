2e43ae409776427f8ac291bf245f5af54d73a770
EricEguski <ericeguskizaga@gmail.com>
2021-01-07 12:48:48 +0100

    barra de busqueda funcional (WIP)

--- resources/views/oca/galeria.blade.php
+++ resources/views/oca/galeria.blade.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="<?php echo asset('css/galeria.css')?>" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src=http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js></script>
 @extends('layouts.master')
 
 @section('content')
@@ -7,7 +9,7 @@
 
     <!-- Search form -->
 <div class="col-md-10 md-form mb-4 mr-auto ml-auto">
-    <input class="form-control" type="text" placeholder="Buscar en nuestra galeria" aria-label="Search">
+    <input class="form-control" id="searchbar" type="text" placeholder="Buscar en nuestra galeria" aria-label="Search">
   </div>
 
 <!-- El div de la galeria -->
@@ -22,7 +24,7 @@
 
                 @php $contador = 0; @endphp <!-- Creamos la variable para romper el foreach -->
                                 
-                @foreach( $mujeres as $key => $a )
+                @foreach( $mujeres as $key => $a)
                 {{-- @for($i=0;$i<=5;$i++) --}}
                     <div class="mujer">
                         <img class="imagen" src="./../resources/img/fotosMujeres/{{$a['imagen']}}" style="height:200px"/>
@@ -44,3 +46,4 @@
     
 
 @stop
+<script src="js/galeria.js"></script>
