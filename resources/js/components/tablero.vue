571050b413b70260032d5d45a32b3b8108ae042b
jessica <jesi7gmar@gmail.com>
2021-01-18 08:02:57 +0100

    18/01/2021

--- resources/js/components/tablero.vue
+++ resources/js/components/tablero.vue
@@ -316,7 +316,6 @@ export default {
   },
   methods: {
     shuffle(a) {
-      //console.log("Me ejecuto");
       var j, x, i;
       for (i = a.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
