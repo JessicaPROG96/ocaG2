2d9f1ffc7cc3d0f6c6d2b2d029b5961a1c20978e
jessica <jesi7gmar@gmail.com>
2021-01-28 14:03:11 +0100

    s

--- resources/views/oca/tablero.blade.php
+++ resources/views/oca/tablero.blade.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="<?php echo asset('css/inicio.css') ?>" type="text/css">
 
 @extends('layouts.master')
+{{-- fgjgfk --}}
 @section('content')
 
 <div id="app" class="content tableroVue">
