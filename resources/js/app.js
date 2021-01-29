0d97075c16829b88f13b95ae98f9d76add4e2c6d
SendoaRios <71374802+SendoaRios@users.noreply.github.com>
2021-01-29 13:37:11 +0100

    oca7

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,10 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+window.Vue = require('vue');
+window.$ = require('jquery');
+window.JQuery = require('jquery');
+Vue.config.devtools = true;
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,8 +22,12 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('clasificacion', require('./components/ClasificacionComponent.vue').default);
 Vue.component('tablero-component', require('./components/tablero.vue').default);
+
+
+ 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +36,10 @@ Vue.component('tablero-component', require('./components/tablero.vue').default);
 
 const app = new Vue({
     el: '#app',
+});
+
+$(document).ready(function(){
+    $("#homePage").on("click",function(){
+        localStorage.removeItem("modoJuego");
+    });
 });
